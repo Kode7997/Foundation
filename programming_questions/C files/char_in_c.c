19b15cb17d4a64b6d35e86faf6b1dd08a3e3ec12
keshav9480 <keshav9480@gmail.com>
2025-01-22 09:09:43 +0530

    commit

--- programming_questions/C files/char_in_c.c
+++ programming_questions/C files/char_in_c.c
@@ -2,7 +2,7 @@
 
 int main(){
     // There is difference between C and C++ when dealing with the char. In C, characters are promoted to int and 
-    // int can be promoted to char but C++ it cannot be done.
+    // int can be promoted to char but in C++ it cannot be done.
 
     //e.g:
     char letter = 'a';
@@ -16,7 +16,6 @@ int main(){
     if (b == let){   //char and int are compared
         printf("Yes a and let are equal\n");
     }
-
 
     return 0;
 }
--- programming_questions/C files/file_handler.cpp
+++ programming_questions/C files/file_handler.cpp
@@ -8,44 +8,45 @@
 7. LRU cache
 */
 
-#include <stdio.h>
+#include<iostream>
+#include <cstdio> 
+#include <cstring> 
+#include <cstdlib>
+
+using namespace std;
 
 void write_and_read_file() {
 
     FILE *fp;
-    fp = fopen("random.txt","rw+");
+    fp = fopen("random.txt","w+");
     if (fp != NULL){
         fprintf(fp, "hello world\n");
         fprintf(fp, "keshav is learning something\n");
         fputs("Hello world\n",fp);
     }
 
-    printf("read from a file\n");
+    
+    /*
+    If following two lines are not there, then program will lead to segm issue. basically reading while fp is open
+    for write.
+    r+ => read if file exist
+    w+ => create and write to file
+    fclose(fp);
+    fp = fopen("random.txt","r+");
+    */
+
+    printf("read from file\n");
     char buffer[100];
     while(fgets(buffer, sizeof(buffer), fp) != NULL) {
-        printf("%s", buffer);
+        //printf("%s", buffer);
+        cout << buffer <<endl;
     }
   
     fclose(fp);
 }
 
-
-void ipc_comm() {
-
-    
-}
-
-
-
-
-
-
 int main(){
     write_and_read_file();
-    ipc_comm()
     return 0;
 
 }
-
-
-
