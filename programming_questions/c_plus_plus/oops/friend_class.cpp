4ed9724a9ef8cbbd394712ca283cd44e8daeb0a3
keshav9480 <keshav9480@gmail.com>
2025-04-01 21:34:52 +0530

    add pure virtual func

--- programming_questions/c_plus_plus/oops/friend_class.cpp
+++ programming_questions/c_plus_plus/oops/friend_class.cpp
@@ -1,8 +1,42 @@
 #include<iostream>
 
+class Base{
+
+    private:
+        int num1;
+
+        void prv_method(){
+            std::cout<<"Base private method\n";
+        }
+        public:
+            Base(int num): num1(num){}
+        
+        
+        friend class Derived; // make derived class as friend of base
+};
+/*
+    - Violates the inheritance properties
+    - Tightly coupled
+*/
+class Derived{
+
+    public:
+
+        Derived(){}
+
+        void call_base_private_func(Base& b){
+            b.prv_method();
+            std::cout<<"Base private number: "<<b.num1<<std::endl;
+        }
+};
+
 int main(){
 
+    Base b(10);
+    Derived d;
 
-    
+    d.call_base_private_func(b);
+
+
     return 0;
 }
